c43a955cd23d1bfef502aef8bd163d485ea8b479
Akash Sharma <sharma.akash1892@gmail.com>
2019-10-08 15:40:58 +0530

    added top 10 ratio emp

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::EmployeesController < ApplicationController
 
+
+	## 2. Create a rest endpoint to print hierarchy given any position in the above mentioned organization (Direction: bottom to top).
   def show
   	employee_hierarchy = Employee.find(params[:id]).path rescue {}
   	render json: {status: "SUCCESS", message: "load employee hierarchy", data: employee_hierarchy}, status: :ok
   end
 
+  ## 1. Create a rest endpoint to input employee details(name ,id, salary,rating etc) in any order (employees might be input before his manager)
   def create
   	response = {}
   	employee = Employee.find(params[:parent_id]) rescue nil
@@ -18,6 +21,13 @@ class Api::V1::EmployeesController < ApplicationController
   	end
   	render json: response, status: (children.present? ? :ok : :unprocessable_entity)
   end
+
+  ## 3. Top 10 employees with ratio of salary
+  def ratio_salary
+  	employees = Employee.order(salary: :desc).limit(10)
+  	render json: {status: "SUCCESS", message: "Top 10 ratio salary employee hierarchy", data: employees}, status: :ok
+  end
+
 
   private
 
