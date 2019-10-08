d24e1d72c6a8c069c2fac46caa56965d0ad5b004
Akash Sharma <sharma.akash1892@gmail.com>
2019-10-08 15:46:12 +0530

    added changes

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -12,11 +12,12 @@ class Api::V1::EmployeesController < ApplicationController
   	response = {}
   	employee = Employee.find(params[:parent_id]) rescue nil
   	if employee.present?
-			children = employee.children.new(name: employee_params[:name], email: employee_params[:email], salary: employee_params[:salary].to_d, rating: employee_params[:rating].to_f, role_id: employee_params[:role_id].to_i)
+  		role = Role.find_by(name: employee_params[:role_name]) rescue nil
+			children = employee.children.new(name: employee_params[:name], email: employee_params[:email], salary: employee_params[:salary].to_d, rating: employee_params[:rating].to_f, role: role)
   		if children.save
   			response = {status: "SUCCESS", message: "employee created under #{employee.email} - #{employee.role.name}", data: children}
   		else
-  			response = {status: "FAILED", message: "employee not saved #{employee.email} - #{employee.role.name}", data: children}
+  			response = {status: "FAILED", message: "#{children.errors.full_messages}", data: children}
   		end
   	end
   	render json: response, status: (children.present? ? :ok : :unprocessable_entity)
@@ -32,7 +33,7 @@ class Api::V1::EmployeesController < ApplicationController
   private
 
   def employee_params
-    params.permit(:name, :email, :salary, :rating, :parent_id, :role_id)
+    params.permit(:name, :email, :salary, :rating, :parent_id, :role_name)
   end
 
 end
