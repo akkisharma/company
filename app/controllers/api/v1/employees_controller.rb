90dbda9d153ce64ca0171b065d45c13339cc50d6
Akash Sharma <sharma.akash1892@gmail.com>
2019-10-08 15:27:02 +0530

    added fix

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -9,20 +9,20 @@ class Api::V1::EmployeesController < ApplicationController
   	response = {}
   	employee = Employee.find(params[:parent_id]) rescue nil
   	if employee.present?
-  		employee_params.delete(:parent_id)
-  		if children = employee.children.create!(employee_params)
+			children = employee.children.new(name: employee_params[:name], email: employee_params[:email], salary: employee_params[:salary].to_d, rating: employee_params[:rating].to_f, role_id: employee_params[:role_id].to_i)
+  		if children.save
   			response = {status: "SUCCESS", message: "employee created under #{employee.email} - #{employee.role.name}", data: children}
   		else
   			response = {status: "FAILED", message: "employee not saved #{employee.email} - #{employee.role.name}", data: children}
   		end
   	end
-  	render json: response, status: :ok
+  	render json: response, status: (children.present? ? :ok : :unprocessable_entity)
   end
 
   private
 
   def employee_params
-    params.require(:employee).permit(:name, :email, :salary, :rating, :parent_id, :role_id)
+    params.permit(:name, :email, :salary, :rating, :parent_id, :role_id)
   end
 
 end
